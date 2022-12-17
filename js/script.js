0400868a9bd5458ef9164614958207176b2f5c03
juanga22 <juanga22072000@gmail.com>
2022-12-16 18:33:09 -0600

    Actualización 16_dic
    Ultima modificación

--- js/script.js
+++ js/script.js
@@ -8,3 +8,13 @@ window.onscroll = function (){
     var elemento1 = document.getElementById("icon_nosotros")
     elemento1.style.bottom = posicion * 0.1 + "px"
 }
+
+// Scroll up
+document.getElementById("button_up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+    if(currentScroll>0){
+        window.scrollTo (0, 0);
+    }
+}
